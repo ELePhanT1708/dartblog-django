ab9b648f17db6f6c9a90685a4b312408de931f65
ELePhanT1708 <elephant1708@gmail.com>
2022-12-05 23:59:30 +0300

    Commit 2 : --- Added static files ith templates and pictures to render on home page

--- dartblog/blog/views.py
+++ dartblog/blog/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+
 # Create your views here.
 
 
-
 def index(request):
-    return HttpResponse('<h1>Ramil Hello !! </h1>')
+    return render(request, 'blog/index.html')
